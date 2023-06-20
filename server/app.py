96a5038facb352a45d540285ca278ab08eb918a0
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-06-20 17:09:56 -0400

    solution

--- server/app.py
+++ server/app.py
@@ -7,8 +7,7 @@ from flask import Flask, make_response, jsonify, request
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 
 app = Flask(__name__)
@@ -20,10 +19,120 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
 @app.route('/')
 def home():
     return ''
+
+class Campers(Resource):
+
+    def get(self):
+        campers = [camper.to_dict() for camper in Camper.query.all()]
+
+        return make_response(campers, 200)
+
+    def post(self):
+        errors=[]
+        try:
+            new_camper = Camper(
+                name=request.json['name'],
+                age=request.json['age']
+            )
+            db.session.add(new_camper)
+            db.session.commit()
+
+            return new_camper.to_dict(), 201
+        except ValueError as e:
+            errors.append(str(e))
+            print(errors)
+            return make_response({"errors": str(e)}, 400)
+
+api.add_resource(Campers, "/campers")
+
+
+class CampersById(Resource):
+
+    def get(self, id):
+        camper = Camper.query.get_or_404(id)
+
+        if camper is None:
+            return make_response({'error': 'Camper not found'}, 404)
+
+        return make_response(camper.to_dict(), 200)
+
+    def patch(self, id):
+        camper = Camper.query.get_or_404(id)
+
+        if camper is None:
+            return {'error': 'Camper not found'}, 404
+
+        try:
+            json_data = request.get_json()
+
+            for key in json_data:
+                setattr(camper, key, json_data.get(key))
+
+            db.session.add(camper)
+            db.session.commit()
+
+            return camper.to_dict(), 202
+
+        except ValueError as e:
+            print(e)
+            return make_response({"errors": ["validation errors"]}, 400)
+
+
+api.add_resource(CampersById, "/campers/<int:id>")
+
+
+class Activities(Resource):
+
+    def get(self):
+        activities = [activity.to_dict() for activity in Activity.query.all()]
+        return activities, 200
+
+
+api.add_resource(Activities, "/activities")
+
+
+class ActivititesById(Resource):
+
+    def delete(self, id):
+        activity = Activity.query.filter_by(id=id).one_or_none()
+
+        if activity:
+            db.session.delete(activity)
+            db.session.commit()
+
+            return make_response({}, 204)
+
+        return make_response({"error": "Activity not found"}, 404)
+
+
+api.add_resource(ActivititesById, "/activities/<int:id>")
+
+
+class Signups(Resource):
+
+    def post(self):
+        try:
+            signup = Signup(
+                time=request.json["time"],
+                camper_id=request.json["camper_id"],
+                activity_id=request.json["activity_id"]
+            )
+
+            db.session.add(signup)
+            db.session.commit()
+
+            return make_response(signup.to_dict(), 201)
+
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+
+
+api.add_resource(Signups, "/signups")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -25,9 +25,11 @@ class Activity(db.Model, SerializerMixin):
     difficulty = db.Column(db.Integer)
 
     # Add relationship
-    
-    # Add serialization rules
-    
+    signups = db.relationship("Signup", cascade="all, delete-orphan", backref="activity")
+    campers = association_proxy('signups', 'camper', creator=lambda c: Signup(camper=c))
+     # Add serialization rules
+    serialize_rules = ("-signups.activity",)
+
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
 
@@ -40,10 +42,29 @@ class Camper(db.Model, SerializerMixin):
     age = db.Column(db.Integer)
 
     # Add relationship
+    signups = db.relationship("Signup", backref="camper")
+    activities = association_proxy('signups', 'activity', creator=lambda a: Signup(activity=a))
     
     # Add serialization rules
+    serialize_rules = ("-signups.camper",)
     
     # Add validation
+    @validates('name')
+    def validate_name(self, key, value):
+        print('Inside the name validation')
+        if not value or len(value) < 1:
+            raise ValueError(f"Must have a {key}.")
+
+        return value
+
+    @validates('age')
+    def validate_age(self, key, value):
+        print('Inside the age validation')
+        if not 8 <= value <= 18:
+            print('Invalid data')
+            raise ValueError(f"Must have an {key} between 8 and 18.")
+
+        return value
     
     
     def __repr__(self):
@@ -57,10 +78,21 @@ class Signup(db.Model, SerializerMixin):
     time = db.Column(db.Integer)
 
     # Add relationships
-    
-    # Add serialization rules
+    camper_id = db.Column(db.Integer, db.ForeignKey("campers.id"))
+    activity_id = db.Column(db.Integer, db.ForeignKey("activities.id"))
+
+     # Add serialization rules
+    serialize_rules = ("-camper.signups", "-activity.signups")
     
     # Add validation
+    @validates('time')
+    def validate_time(self, key, value):
+        print('Inside the time validation')
+        if not 0 <= value <= 23:
+            print('Invalid data')
+            raise ValueError(f"{key} must be between 0 and 23.")
+
+        return value
     
     def __repr__(self):
         return f'<Signup {self.id}>'
